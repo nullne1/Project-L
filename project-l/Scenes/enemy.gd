e2a467e875d23d37da8f6ee20522ae0dfa185a83
nullne1 <nullne@hotmail.com>
2025-06-14 13:44:15 -0400

    added skeletons, bugfixes

--- project-l/Scenes/enemy.gd
+++ project-l/Scenes/enemy.gd
@@ -12,6 +12,7 @@ const SWORD_SLASH = preload("res://Scenes/sword_slash.tscn")
 
 var ms := 200;
 var in_range := false;
+var last_direction: String;
 
 func _ready() -> void:
 	await get_tree().create_timer(2).timeout;
@@ -23,15 +24,30 @@ func _physics_process(delta: float) -> void:
 		velocity = (player.position - position).normalized() * ms;
 		
 	if (position.distance_to(player.position) > 3):
+		play_direction(player.position, "walk");
 		move_and_collide(delta * velocity);
-	
-	if (position.x < player.position.x):
-		sprite.flip_h = false;
-	elif (position.x >= player.position.x):
-		sprite.flip_h = true;
 	
 	if (hp <= 0):
 		queue_free();
+
+func play_direction(direction_target, animation) -> void:
+	var angle := rad_to_deg(position.angle_to_point(direction_target));
+	# up
+	if (angle > -120 && angle < -60):
+		sprite.play(animation + "_u");
+		last_direction = "_u"
+	# left
+	elif (angle < -120 && angle > -180 || angle < 180 && angle > 120):
+		sprite.play(animation + "_l");
+		last_direction = "_l"
+	# down
+	elif (angle < 120 && angle > 60):
+		sprite.play(animation + "_d");
+		last_direction = "_d"
+	# right
+	elif (angle < 60 && angle > 0 || angle < 0 && angle > -60):
+		sprite.play(animation + "_r");
+		last_direction = "_r"
 
 func _on_attack_range_body_entered(body: Node2D) -> void:
 	if (body == player):
