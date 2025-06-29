8aa5aed1aaf8fed60f7dfcec1f57815baa2db35a
nullne1 <nullne@hotmail.com>
2025-06-29 03:32:15 -0400

    added enemy attacking and death

--- project-l/Scenes/enemy.gd
+++ project-l/Scenes/enemy.gd
@@ -1,59 +1,99 @@
 extends CharacterBody2D
 
-const CHARGE_UP_ZOMBIE = preload("res://Assets/topdown_textures/charge_up_zombie.png")
-const ATTACK_ZOMBIE = preload("res://Assets/topdown_textures/attack_zombie.png")
-const SWORD_SLASH = preload("res://Scenes/sword_slash.tscn")
+const SWORD_SLASH_PATH = preload("res://Scenes/sword_slash.tscn")
 
 @onready var hp := 100;
 @onready var player: CharacterBody2D = $"../../User/Player"
 @onready var asprite: Sprite2D = $Sprite2D
 @onready var enemy_health_bar: TextureProgressBar = %EnemyHealthBar
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var attack_speed_cd: Timer = $AttackSpeedCD
 
 #var ms := 200;
-var ms := 150;
+var ms := 250;
 var in_range := false;
 var last_direction: String;
+var attack_finished: bool = true;
+var sword_slash_area: Area2D;
 
 func _ready() -> void:
-	await get_tree().create_timer(2).timeout;
+	attack_speed_cd.wait_time = 2;
 
 func _physics_process(delta: float) -> void:
-	enemy_health_bar.value = hp;	
-	
-	if (!in_range):
-		velocity = (player.position - position).normalized() * ms;
+	enemy_health_bar.value = hp;
+	# keeps attacking if player in range
+	if (in_range && attack_speed_cd.is_stopped()):
+		attack_finished = false;
+		play_direction_attack(player.position, "attack");
 		
-	if (position.distance_to(player.position) > 3):
+	if (in_range):
+		velocity = Vector2.ZERO;
+	elif (position.distance_to(player.position) > 3 && attack_finished):
+		velocity = (player.position - position).normalized() * ms;
 		play_direction(player.position, "walk");
 		move_and_collide(delta * velocity);
-	
+		
 	if (hp <= 0):
 		queue_free();
 
-func play_direction(direction_target, animation) -> void:
+func sword_attack() -> void:
+	sword_slash_area = SWORD_SLASH_PATH.instantiate();
+	add_child(sword_slash_area);
+	sword_slash_area.rotation_degrees = rad_to_deg(get_angle_to(player.position)) - 90;
+	sword_slash_area.global_position = global_position;
+	attack_speed_cd.start();
+	
+func play_direction(direction_target: Vector2, animation: String) -> void:
 	var angle := rad_to_deg(position.angle_to_point(direction_target));
-	# up
 	if (angle > -120 && angle < -60):
 		sprite.play(animation + "_u");
 		last_direction = "_u"
-	# left
 	elif (angle < -120 && angle > -180 || angle < 180 && angle > 120):
 		sprite.play(animation + "_l");
 		last_direction = "_l"
-	# down
 	elif (angle < 120 && angle > 60):
 		sprite.play(animation + "_d");
 		last_direction = "_d"
-	# right
 	elif (angle < 60 && angle > 0 || angle < 0 && angle > -60):
 		sprite.play(animation + "_r");
 		last_direction = "_r"
 
+func play_direction_attack(direction_target: Vector2, animation: String) -> void:
+	var angle := rad_to_deg(position.angle_to_point(direction_target));
+	if (angle > -150 && angle < -30):
+		sprite.play(animation + "_u");
+		last_direction = "_u"
+	elif (angle < -150 && angle > -180 || angle < 180 && angle > 150):
+		sprite.play(animation + "_l");
+		last_direction = "_l"
+	elif (angle < 150 && angle > 30):
+		sprite.play(animation + "_d");
+		last_direction = "_d"
+	elif (angle < 30 && angle > 0 || angle < 0 && angle > -30):
+		sprite.play(animation + "_r");
+		last_direction = "_r"
+		
 func _on_attack_range_body_entered(body: Node2D) -> void:
-	if (body == player):
-		#sprite.texture = CHARGE_UP_ZOMBIE;
-		var attack = SWORD_SLASH.instantiate() as StaticBody2D;
-		add_child(attack);
+	if (body == player && attack_speed_cd.is_stopped()):
+		attack_finished = false;
+		play_direction_attack(player.position, "attack");
 		in_range = true;
-		velocity = Vector2(0, 0);
+	elif (body == player):
+		in_range = true;
+		
+func _on_attack_range_body_exited(body: Node2D) -> void:
+	if (body == player):
+		in_range = false;
+	
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if (sprite.animation == "attack_u" || sprite.animation == "attack_l" || sprite.animation == "attack_d" || sprite.animation == "attack_r"): 
+		sprite.play("idle" + last_direction)
+		sword_slash_area.find_child("CollisionShape2D").queue_free();
+		sword_slash_area.queue_free();
+		attack_finished = true;
+
+# spawn sword attack on frame 4 of attack
+func _on_animated_sprite_2d_frame_changed() -> void:
+	if (sprite.frame == 4 && (sprite.animation == "attack_u" || sprite.animation == "attack_l" || sprite.animation == "attack_d" || sprite.animation == "attack_r")): 
+		sword_attack();
+		
--- project-l/Scenes/target.gd
+++ project-l/Scenes/target.gd
@@ -1,7 +1,8 @@
 extends Area2D
 @onready var player: CharacterBody2D = $"../Player"
+@onready var idle_area: Area2D = $"../Player/IdleArea"
 
-func _on_body_entered(body: Node2D) -> void:
-	if (body == player):
+func _on_area_entered(area: Area2D) -> void:
+	if (area == idle_area):
 		player.sprite.play("idle" + player.last_direction);
 		queue_free();
