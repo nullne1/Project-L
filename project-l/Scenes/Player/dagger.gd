ee5561501bf29b2e2c1a55cbc2d9d264ccf9167e
nullne1 <nullne@hotmail.com>
2025-09-05 12:46:49 -0400

    projectile class

--- project-l/Scenes/Player/dagger.gd
+++ project-l/Scenes/Player/dagger.gd
@@ -1,20 +1,8 @@
-extends CharacterBody2D
+extends Projectile
 
-signal enemy_to_hit_death;
-signal hit_enemy_to_hit;
 signal add_player_xp;
 
-@onready var tile_map: TileMapLayer = $"../../Map/TileMapLayer"
-@onready var area_2d: Area2D = $Area2D
-@onready var player: CharacterBody2D = $"../Player"
 @onready var dagger_icon: TextureProgressBar = $"../../Control/CanvasLayer/TextureRect/HBoxContainer/DaggerIcon"
-
-var pos : Vector2;
-var rota : float;
-var direction : float;
-var speed = 450;
-var enemy_to_hit: CharacterBody2D;
-
 
 func ready() -> void:
 	global_position = pos;
@@ -31,7 +19,7 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		queue_free();
 	elif (body == enemy_to_hit):
 		queue_free();
-		if (enemy_to_hit.hp - 20 >= 20):
+		if (enemy_to_hit.hp - player.dmg >= player.dmg):
 			hit_enemy_to_hit.connect(enemy_to_hit.on_hit);
 			hit_enemy_to_hit.emit();
 		else:
@@ -41,6 +29,7 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 			add_player_xp.emit();
 			enemy_to_hit_death.connect(enemy_to_hit.on_death);
 			enemy_to_hit_death.emit();
+		
 			
 			
 	
